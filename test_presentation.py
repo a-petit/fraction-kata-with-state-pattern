bbcf4844b6adc9b0aa00eb5b149f8fe5814b544e
lxnd <lxnd@protonmail.com>
2023-11-07 16:16:47 +0100

    feat: regular fraction (triangulé)

--- test_presentation.py
+++ test_presentation.py
@@ -1,11 +1,24 @@
+from dataclasses import dataclass
+
+import pytest
+
+
+@dataclass(frozen=True)
 class Fraction:
+    _numerator: int
+    _denominator: int
+
     @classmethod
     def of(cls, numerator, denominator):
-        return Fraction()
+        return Fraction(numerator, denominator)
 
     def representation(self) -> str:
-        return "1/3"
+        return f"{self._numerator}/{self._denominator}"
 
 
-def test_regular_fraction():
-    assert Fraction.of(1, 3).representation() == "1/3"
+@pytest.mark.parametrize("numerator, denominator, representation", [
+    (1, 3, "1/3"),
+    (3, 7, "3/7"),
+])
+def test_regular_fraction(numerator, denominator, representation):
+    assert Fraction.of(numerator, denominator).representation() == representation
