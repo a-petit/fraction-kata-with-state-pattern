a1cef3db0f930c75353262b6a96338b4b153dbb6
lxnd <lxnd@protonmail.com>
2023-11-07 16:19:29 +0100

    feat: display negative fractions

--- test_presentation.py
+++ test_presentation.py
@@ -10,6 +10,9 @@ class Fraction:
 
     @classmethod
     def of(cls, numerator, denominator):
+        if denominator < 0:
+            numerator *= -1
+            denominator *= -1
         return Fraction(numerator, denominator)
 
     def representation(self) -> str:
@@ -17,8 +20,13 @@ class Fraction:
 
 
 @pytest.mark.parametrize("numerator, denominator, representation", [
+    # Fractions basiques
     (1, 3, "1/3"),
     (3, 7, "3/7"),
+    # Fractions négatives
+    (-1, 4, "-1/4"),
+    (1, -4, "-1/4"),
+    (-1, -4, "1/4"),
 ])
 def test_regular_fraction(numerator, denominator, representation):
     assert Fraction.of(numerator, denominator).representation() == representation
